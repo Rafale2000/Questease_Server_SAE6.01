fc6b3638038752310b1a4495ccc9efac9b34a7ed
theo <102145780+letheos@users.noreply.github.com>
2024-12-05 10:56:51 +0100

    sauvegarde

--- src/main/java/fr/uphf/Questease/Controller/Lobby.java
+++ src/main/java/fr/uphf/Questease/Controller/Lobby.java
@@ -1,45 +1,81 @@
 package fr.uphf.Questease.Controller;
 
-import fr.uphf.Questease.Model.Utilisateur;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 
-public class Lobby extends Thread{
-    private Utilisateur user1 = null;
-    private boolean estPretUser1 = false;
-    private Utilisateur user2 = null;
-    private boolean estPretUser2 = false;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
-    public Lobby(){
+public class Lobby {
+    /*private static int counter = 0;
+    private final int id;
+    */
+    private String nom;
+    private User p1;
+    private User p2;
+    private ArrayList<String> lobbygames;
 
-    }
-    public Lobby(Utilisateur user1){
-        this.user1 = user1;
-    }
 
-    public void Connexion(Utilisateur user){
-        //verifier la connexion
-
-    }
-
-    public void Connexion(String pseudo){
-        //verifier la connexion
-    }
-
-    public boolean Ready(){
-        return this.user1 != null && this.user2 != null;
-    }
-
-    public void RendrePret(Utilisateur user){
-        if(user == this.user1){
-            this.estPretUser1 = true;
-        } else if(user == this.user2){
-            this.estPretUser2 = true;
-        }
+    public Lobby(User user,String nom) {
+        //this.id = (++counter);
+        this.nom = nom;
+        this.p1 = user;
+        this.p2 = null;
+        ArrayList<String> possiblegames = new ArrayList<String>();
+        //possiblegames.addAll(Arrays.asList("pendu","prix_juste","rotating_pictures","menteur","cryptex"));
+        possiblegames.addAll(Arrays.asList("prix_juste"));
+        this.lobbygames= new ArrayList<>();
+        Collections.shuffle(possiblegames);
+        /*for(int x=0;x<3;x++){
+            this.lobbygames.add(possiblegames.get(x));
+        }*/
+        this.lobbygames.addAll(possiblegames);
     }
 
-    public void Lauch(){
-        //TODO
-        //créer une boucle et fait les jeux à la suite
-        //fin de game ajouter le résultat à la bdd
+    public String getNextGame(){
+        return this.lobbygames.get(0);
     }
-    
+    private void addplayer(User user){
+        this.p2 = user;
+    }
+    private void sendtoplayers(String message) throws IOException {
+        TextMessage content = new TextMessage(message);
+        WebSocketSession webSocketSession1 = p1.getSession();
+        webSocketSession1.sendMessage(content);
+        WebSocketSession webSocketSession2 = p1.getSession();
+        webSocketSession2.sendMessage(content);
+    }
+    private ArrayList<WebSocketSession> getplayers(){
+        ArrayList<WebSocketSession> playerlist = new ArrayList<>();
+        return playerlist;
+    }
+    public String getNom(){
+        return this.nom;
+    }
+
+    public User getp1(){
+        return this.p1;
+    }
+    public User getp2(){
+
+        return this.p2;
+    }
+    public void setp2(User user){
+        this.p2 = user;
+    }
+    public void setp1(User user){
+        this.p1 = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Lobby{" +
+                "nom='" + nom + '\'' +
+                ", p1=" + p1 +
+                ", p2=" + p2 +
+                ", lobbygames=" + lobbygames +
+                '}';
+    }
 }
