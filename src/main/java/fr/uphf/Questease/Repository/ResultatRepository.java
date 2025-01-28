becc8fed96679169277923781082a0b754b2f7ba
Rafale2000 <loick1morneau@icloud.com>
2025-01-28 10:58:18 +0100

    ajout de nouvelles classe: - ResultatRepository.java - ResultatService.java

---
+++ src/main/java/fr/uphf/Questease/Repository/ResultatRepository.java
@@ -0,0 +1,7 @@
+package fr.uphf.Questease.Repository;
+
+import fr.uphf.Questease.Model.Resultat;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ResultatRepository extends CrudRepository<Resultat, Long> {
+}
