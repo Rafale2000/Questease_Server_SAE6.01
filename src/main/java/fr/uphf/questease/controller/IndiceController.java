d5593c3d6f3b1287d28c3a82d6f80664345f63cd
Rafale2000 <loick1morneau@icloud.com>
2025-01-28 15:52:06 +0100

    reffracor du code

--- src/main/java/fr/uphf/questease/controller/IndiceController.java
+++ src/main/java/fr/uphf/questease/controller/IndiceController.java
@@ -2,7 +2,9 @@ package fr.uphf.questease.controller;
 
 import fr.uphf.questease.model.Indice;
 import fr.uphf.questease.service.IndiceServiceImpl;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,9 +34,9 @@ public class IndiceController {
      * @param idIndice L'id de l'indice à récupérer
      * @return L'indice qui possède l'id précedemment mentionné
      */
-    @GetMapping("/idIndice")
+    @GetMapping("/idIndice{idIndice}")
     public Optional<Indice> getIndiceById(@PathVariable Long idIndice) {
-        return repo.FetchIndice(idIndice);
+        return repo.fetchIndice(idIndice);
     }
 
     /**
@@ -43,45 +45,51 @@ public class IndiceController {
      */
     @GetMapping("")
     public List<Indice> getAllIndices() {
-        return repo.FetchIndiceList();
+        return repo.fetchIndiceList();
     }
 
     /**
      * Méthode Post permettant d'ajouter à la base de donnée un indice
-     * @param I L'indice à ajouter à la base de donnée
+     * @param indice L'indice à ajouter à la base de donnée
      */
-    @PostMapping("/{idIndice}")
-    public void PostIndice(@PathVariable Indice I) {
-        repo.SaveIndice(I);
+    @PostMapping("/get{idIndice}")
+    public void postIndice(@PathVariable("idIndice") Long idIndice, @RequestBody Indice indice) {
+        indice.setId(idIndice);
+        repo.saveIndice(indice);
     }
 
     /**
      * Méthode Update permettant de mettre à jour un indice de la base de donnée
-     * @param IdIndice L'id de l'indice à mettre à jour
-     * @param I L'indice qui remplacera celui mis à jour
+     * @param idIndice L'id de l'indice à mettre à jour
+     * @param updatedIndice L'indice qui remplacera celui mis à jour
      */
     @PatchMapping("/{idIndice}")
-    public void UpdateIndice(Long IdIndice, @PathVariable Indice I) {
-        repo.deleteIndice(IdIndice);
-        repo.SaveIndice(I);
+    public void updateIndice(@PathVariable("idIndice") Long idIndice, @RequestBody Indice updatedIndice) {
+        Indice existingIndice = repo.fetchIndice(idIndice).orElseThrow(() -> new RuntimeException("Indice introuvable"));
+        existingIndice.setIndiceText(updatedIndice.getHint());
+        repo.saveIndice(existingIndice);
     }
 
     /**
      * Méthode Delete permettant de supprimer un Indice de la base de donnée
-     * @param I L'indice supprimé de la base de donnée
+     * @param idIndice L'indice supprimé de la base de donnée
      */
     @DeleteMapping("/{idIndice}")
-    public void DeleteinfoSecu(@PathVariable Indice I) {
-        repo.deleteIndice(I.getId());
+    public void deleteinfoSecu(@PathVariable int idIndice) {
+        repo.deleteIndice((long) idIndice);
     }
 
     /**
-     * Méthode Get premettant de récupérer un indice avec son id
-     * @param idIndice
-     * @return
+     * Méthode GET permettant de récupérer un indice avec son ID.
+     *
+     * @param idIndice ID de l'indice à récupérer.
+     * @return L'objet Indice correspondant à l'ID.
+     * @throws ResponseStatusException si l'ID est introuvable.
      */
     @GetMapping("/{idIndice}")
-    public Indice GetIndice(@PathVariable Long idIndice) {
-        return repo.FetchIndice(idIndice).get();
+    public Indice getIndice(@PathVariable Long idIndice) {
+        return repo.fetchIndice(idIndice)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Indice introuvable pour l'ID : " + idIndice));
     }
+
 }
